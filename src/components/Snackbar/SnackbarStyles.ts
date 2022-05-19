2f1ae6fa5ff3e46bc62f9e9efd65ca85a78962a1
Shashank Trivedi <51978440+mdiffshashank@users.noreply.github.com>
2022-05-19 23:53:18 +0530

    Create SnackbarStyles.ts

---
+++ src/components/Snackbar/SnackbarStyles.ts
@@ -0,0 +1,33 @@
+import { makeStyles } from "@mui/styles";
+import {  Theme } from "@mui/material";
+import { green, amber } from "@mui/material/colors";
+
+
+export default makeStyles((theme: Theme) => ({
+  success: {
+    backgroundColor: green[600],
+  },
+  error: {
+    backgroundColor: theme.palette.error.dark,
+  },
+  info: {
+    backgroundColor: theme.palette.primary.main,
+  },
+  warning: {
+    backgroundColor: amber[700],
+  },
+  icon: {
+    fontSize: 20,
+  },
+  iconVariant: {
+    opacity: 0.9,
+    marginRight: theme.spacing(1),
+  },
+  message: {
+    display: "flex",
+    alignItems: "center",
+  },
+  margin: {
+    margin: theme.spacing(1),
+  },
+}));
